cd61466066ead448fc63d116054a2ea78615f90b
Thomas Antony <mail@thomasantony.com>
2023-04-11 21:04:50 -0700

    Update CMakeLists.txt and get_sqlite.sh

--- vendor/get_sqlite.sh
+++ vendor/get_sqlite.sh
@@ -2,8 +2,9 @@
 set -euo pipefail
 
 VENDOR_DIR=$(dirname "$0")
-
+echo "installing to $VENDOR_DIR"
 wget https://www.sqlite.org/2022/sqlite-autoconf-3400100.tar.gz -O $VENDOR_DIR/sqlite.tar.gz
 tar -xvzf $VENDOR_DIR/sqlite.tar.gz
 rm $VENDOR_DIR/sqlite.tar.gz
+rm -rf $VENDOR_DIR/sqlite
 mv sqlite-autoconf-3400100/ $VENDOR_DIR/sqlite
